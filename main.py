ee49bdd6171167fef699e7e0c58294f759309f69
LeonVilovic <vilovic.leon@gmail.com>
2025-06-23 12:30:49 +0200

    create multiple images

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 from PIL import Image
+import numpy as np
+import random
 
-if __name__ == "__main__":
-
-    from PIL import Image
-    import numpy as np
-    import random
-
-    image_path = r'input.png'
-    input_image = Image.open(image_path).convert('RGB')  # Convert to RGB
-    # input_image.show()
-
-    # Convert to NumPy array for easy pixel access
+def pixely_effect(n, input_path, output_path):
+    input_image = Image.open(input_path).convert('RGB')
     input_pixels = np.array(input_image)
 
     height, width, _ = input_pixels.shape
-    output_size = (width, height)
-    output_image = Image.new('RGB', output_size)
-
-    # adjust n for more or less pixely effect
-    n = 5
+    output_image = Image.new('RGB', (width, height))
 
     for y in range(0, height - n, n):
         for x in range(0, width - n, n):
-            rand_x = random.randint(x, x + n)
-            rand_y = random.randint(y, y + n)
+            rand_x = random.randint(x, x + n - 1)
+            rand_y = random.randint(y, y + n - 1)
             pixel = tuple(input_pixels[rand_y, rand_x])
-            for i in range(0, n, 1):
-                for j in range(0, n, 1):
-                    output_image.putpixel((x + i, y + j), pixel)
+            for i in range(n):
+                for j in range(n):
+                    if x + i < width and y + j < height:
+                        output_image.putpixel((x + i, y + j), pixel)
 
-    output_image.save('output.png')
-    output_image.show()
+    output_image.save(output_path)
+    #output_image.show()
+
+if __name__ == "__main__":
+    for i in range(0, 10, 1):
+        output_filename = f'output{i}.png'
+        pixely_effect(4, 'input.png', output_filename)
+
+
